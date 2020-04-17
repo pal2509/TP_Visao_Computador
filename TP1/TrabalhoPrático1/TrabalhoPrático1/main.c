0baf495ca7a13f5237a731563638128aab0fea47
EnmanuelVzla <56695766+EnmanuelVzla@users.noreply.github.com>
2020-04-17 11:44:15 +0100

    adicione 2 funciones para gerar os perfiles das linhas e columnas de uma imagen

--- TP1/TrabalhoPrático1/TrabalhoPrático1/main.c
+++ TP1/TrabalhoPrático1/TrabalhoPrático1/main.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
 #include "vc.h"
 
+int vc_perfil_columna(IVC *src, IVC *dst) {
 
+    int x, y, soma, pos_src, pos_dst;
+
+    for (x = 0; x < src->width; x++)
+    {
+        soma = 0;
+
+        for (y = 0; y < src->height; y++)
+        {
+            pos_src = y * src->bytesperline + x;
+
+            soma += src->data[pos_src];
+        }
+
+        pos_dst = (dst->height-1-(soma / y)) * dst->bytesperline +x;
+        dst->data[pos_dst] = 1;
+    }
+
+
+    return 0;
+}
+
+int vc_perfil_rows(IVC *src, IVC *dst) {
+
+    int x, y, soma, pos_src, pos_dst;
+
+    for (y = 0; y < src->height; y++)
+    {
+        soma = 0;
+
+         for (x = 0; x < src->width; x++)
+        {
+            pos_src = y * src->bytesperline + x;
+
+            soma += src->data[pos_src];
+        }
+
+        pos_dst = y * dst->bytesperline +(soma/x);
+        dst->data[pos_dst] = 1;
+    }
+
+
+    return 0;
+}
 
 int main(void)
 {
@@ -23,5 +67,14 @@ int main(void)
 	printf("press any key to exit...\n");
 	getchar();
 
+	/*
+	    new_image = vc_image_new(image->width, 256, 1, 1);
+	    vc_perfil_columna(image, new_image);
+	    vc_write_image("output/perfil_columna.pbm", new_image);
+
+	    new_image2 = vc_image_new(256, image->height, 1, 1);
+	    vc_perfil_rows(image, new_image2);
+	    vc_write_image("output/perfil_rows.pbm", new_image2);
+	*/
+	
 	return 0;
-}
